0bca2668564e09c31d1f31b0bed8c7654a363dc7
vikram <vikramsingla2002@gmail.com>
2022-07-03 11:27:42 +0530

    first commit

---
+++ bst.js
@@ -0,0 +1,353 @@
+let setsize = document.querySelector('#setsize');
+let n , m;
+setsize.addEventListener("click" , function(){    
+    n = document.querySelector("#sizen").value;
+    n -= 0;
+    m = document.querySelector("#sizem").value;
+    m -= 0;
+    console.log(n , m);
+    createGrid(n , m);
+});
+
+function createGrid(n , m){
+    var maze = new Array(n);
+    var visited=new Array(n);
+    for (var i = 0; i < maze.length; i++) {
+    maze[i] = new Array(m);
+    visited[i]=new Array(m);
+    }
+    const cont = document.createElement("container");
+    cont.style.background = "white";
+    cont.style.display = "inline-block";
+    cont.style.border = "0px solid #252423";
+    const body = document.body;
+    body.append(cont);
+    let rows = "";
+    for(let i = 0 ; i < n ; i++) {
+    rows += "20px ";
+    }
+    let cols = "";
+    for(let i = 0 ; i < m ; i++) {
+    cols += "20px ";
+    }
+
+    const grid = document.createElement("grid");
+    grid.id = "yo1";
+    grid.style.display = "grid";
+    grid.style.gridTemplateColumns = cols;
+    grid.style.gridTemplateRows = rows;
+    grid.style.gap = "0px";
+    body.append(grid);
+    cont.append(grid);
+
+    let k ;
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<m; j++) {
+        visited[i][j]=0;
+            k = document.createElement("cell");
+            k.id=`${i * m + j}`;
+            maze[i][j] = k;
+            maze[i][j].classList.add("grid");
+            body.append(k);
+            cont.append(k);
+            grid.append(k);
+            k.style.border = "1px solid #252423";
+            k.style.justifyContent = "center";
+            k.style.alignItems = "center";
+            k.style.display = "flex";
+            k.style.fontSize= "3rem";
+            k.style.fontWeight = "bold";
+            k.style.background = "#252423";
+            k.className="cell";
+        }
+    }
+
+    const yo = document.querySelector('.left');
+    yo.innerHTML += cont.innerHTML;
+    cont.innerHTML = "";
+}
+
+let ord = [];
+
+
+
+function southeast() {
+    for(let i = 0 ; i < n ; i++) {
+        for(let j = 0; j < m ; j++) {
+            if(i == n - 1 && j == m - 1) continue;
+            let choice = Math.ceil(Math.random() * 2);
+            //1 -> right
+            let flag = false;
+            if(choice == 1) {
+     ;
+                 if(j + 1 < m) {
+     ;
+                     ord.push([[i , j ] ,[i , j + 1]]);
+     ;
+                     flag = true;
+                     
+                }
+
+                if(!flag) {
+                    ord.push([[i , j ] ,[i + 1 , j]]);
+                }
+            }
+            else{
+                if(i + 1 < n) {
+                    ord.push([[i , j ] ,[i + 1, j]]);
+                    flag = true;
+                }
+                if(!flag) {
+                    ord.push([[i , j ] ,[i , j + 1]]);
+                }
+            }
+        }
+    }
+
+    console.log(ord);
+}
+
+function northeast() {
+    for(let i = 0 ; i < n ; i++) {
+        for(let j = 0; j < m ; j++) {
+            if(i == 0 && j == m - 1) continue;
+            let choice = Math.ceil(Math.random() * 2);
+            //1 -> right
+            let flag = false;
+            if(choice == 1) {
+                if(j + 1 < m) {
+                    ord.push([[i , j ] ,[i , j + 1]]);
+                    flag = true;
+                }
+
+                if(!flag) {
+                    ord.push([[i , j ] ,[i - 1 , j]]);
+                }
+            }
+            else{
+                if(i - 1 >= 0) {
+                    ord.push([[i , j ] ,[i - 1, j]]);
+                    flag = true;
+                }
+                if(!flag) {
+                    ord.push([[i , j ] ,[i , j + 1]]);
+                }
+            }
+        }
+    }
+    console.log(ord);
+}
+
+function northwest() {
+    for(let i = 0 ; i < n ; i++) {
+        for(let j = 0; j < m ; j++) {
+            if(i == 0 && j == 0) continue;
+            let choice = Math.ceil(Math.random() * 2);
+            //1 -> left
+            let flag = false;
+            if(choice == 1) {
+                if(j - 1 >= 0) {
+                    ord.push([[i , j ] ,[i , j - 1]]);
+                    flag = true;
+                }
+
+                if(!flag) {
+                    ord.push([[i , j ] ,[i - 1 , j]]);
+                }
+            }
+            else{
+                if(i - 1 >= 0) {
+                    ord.push([[i , j ] ,[i - 1, j]]);
+                    flag = true;
+                }
+                if(!flag) {
+                    ord.push([[i , j ] ,[i , j - 1]]);
+                }
+            }
+        }
+    }
+    console.log(ord);
+}
+
+function southwest() {
+    for(let i = 0 ; i < n ; i++) {
+        for(let j = 0; j < m ; j++) {
+            if(i == n - 1 && j == 0) continue;
+            let choice = Math.ceil(Math.random() * 2);
+            //1 -> left
+            let flag = false;
+            if(choice == 1) {
+                if(j - 1 >= 0) {
+                    ord.push([[i , j ] ,[i , j - 1]]);
+                    flag = true;
+                }
+
+                if(!flag) {
+                    ord.push([[i , j ] ,[i + 1 , j]]);
+                }
+            }
+            else{
+                if(i + 1 < n) {
+                    ord.push([[i , j ] ,[i + 1, j]]);
+                    flag = true;
+                }
+                if(!flag) {
+                    ord.push([[i , j ] ,[i , j - 1]]);
+                }
+            }
+        }
+    }
+    console.log(ord);
+}
+
+
+function animate(index){
+  let sz=ord.length;
+  if (index==sz) return;
+  let cidc=ord[index][0];
+  let parentc;
+  parentc=ord[index][1];
+
+  let cid=cidc[0]*m+cidc[1];
+  let parent
+  parent=parentc[0]*m+parentc[1];
+
+
+  let borlef = document.getElementById(`${cid}`).style.borderLeft;
+  let borri = document.getElementById(`${cid}`).style.borderRight;
+  let borto = document.getElementById(`${cid}`).style.borderTop;
+  let borbo = document.getElementById(`${cid}`).style.borderBottom;
+
+  let Pborlef = document.getElementById(`${parent}`).style.borderLeft;
+  let Pborri = document.getElementById(`${parent}`).style.borderRight;
+  let Pborto = document.getElementById(`${parent}`).style.borderTop;
+  let Pborbo = document.getElementById(`${parent}`).style.borderBottom;
+
+  let direction;
+  if (cidc[0]-parentc[0]==1) direction=2;
+  if (cidc[0]-parentc[0]==-1) direction=1;
+  if (cidc[1]-parentc[1]==1) direction=4;
+  if (cidc[1]-parentc[1]==-1) direction=3;
+
+  if(direction == 1) { 
+    borbo = "0px solid black ";
+  }
+  if(direction == 2) { 
+    borto = "0px solid black ";
+  } 
+  if(direction == 3) { 
+    borri = "0px solid black ";
+  } 
+  if(direction == 4) { 
+    borlef = "0px solid black ";
+  }  
+
+  if(direction == 1) { 
+    Pborto = "0px solid black";
+  }
+  if(direction == 2) { 
+    Pborbo = "0px solid black";
+  }
+  if(direction == 3) { 
+    Pborlef = "0px solid black";
+  }
+  if(direction == 4) { 
+    Pborri = "0px solid black";
+  }
+
+  let abcd=document.getElementById(`${parent}`);
+
+  var also1 = function () {anime({
+    targets : abcd,
+    backgroundColor : '#00D672',
+    borderBottom : Pborbo,
+    borderTop : Pborto,
+    borderLeft : Pborlef,
+    borderRight : Pborri,
+    duration : 10,
+    complete: () => {
+      myanimation();
+    }
+  }).play()};
+
+  var myanimation = function () {anime({
+    targets: document.getElementById(`${cid}`),
+    backgroundColor : '#00D672',
+    scale: [
+      {value: .1, easing: 'easeOutSine', duration: 10},
+      {value: 1, easing: 'easeInOutQuad', duration: 10}
+    ],
+    borderBottom : borbo,
+    borderTop : borto,
+    borderLeft : borlef,
+    borderRight : borri,
+    duration : 5,
+    delay : 1,
+    complete: ()=> {
+      if(index + 1 == sz) return;
+      animate(index + 1);
+  }}).play()};
+  console.log(true);
+  also1();
+  // myanimation.play();
+}
+
+let seb = false;
+let neb = false;
+let nwb = false;
+let swb = false;
+
+let nore = document.querySelector('.northeast');
+let soue = document.querySelector('.southeast');
+let norw = document.querySelector('.northwest');
+let souw = document.querySelector('.southwest');
+
+nore.addEventListener("click" , function() {
+    seb = false;
+    neb = true;
+    nwb = false;
+    swb = false;
+    nore.style.backgroundColor = "#343434";
+    nore.style.border = "2px solid white";
+})
+
+soue.addEventListener("click" , function() {
+    seb = true;
+    neb = false;
+    nwb = false;
+    swb = false;
+    soue.style.backgroundColor = "#343434";
+    soue.style.border = "2px solid white";
+})
+
+norw.addEventListener("click" , function() {
+    seb = false;
+    neb = false;
+    nwb = true;
+    swb = false;
+    norw.style.backgroundColor = "#343434";
+    norw.style.border = "2px solid white";
+})
+
+souw.addEventListener("click" , function() {
+    seb = false;
+    neb = false;
+    nwb = false;
+    swb = true;
+    souw.style.backgroundColor = "#343434";
+    souw.style.border = "2px solid white";
+})
+
+const button = document.querySelector("#go");
+
+button.addEventListener("click" , function() {
+
+    if(neb) northeast();
+    if(seb) southeast();
+    if(swb) southwest();
+    if(nwb) northwest();
+
+    setTimeout(function(){
+        animate(0);
+    } , 5000);
+})
---
+++ index.js
@@ -0,0 +1,230 @@
+let n = 40, m = 40;
+var maze = new Array(n);
+var visited=new Array(n);
+for (var i = 0; i < maze.length; i++) {
+  maze[i] = new Array(m);
+  visited[i]=new Array(m);
+}
+const cont = document.createElement("container");
+cont.style.background = "white";
+cont.style.display = "inline-block";
+cont.style.border = "0px solid #252423";
+// cont.style.marginLeft = "20px";
+// cont.style.marginTop = "20px";
+const body = document.body;
+body.append(cont);
+let rows = "";
+for(let i = 0 ; i < n ; i++) {
+  rows += "20px ";
+}
+let cols = "";
+for(let i = 0 ; i < m ; i++) {
+  cols += "20px ";
+}
+
+const grid = document.createElement("grid");
+grid.id = "yo1";
+grid.style.display = "grid";
+grid.style.gridTemplateColumns = cols;
+grid.style.gridTemplateRows = rows;
+grid.style.gap = "0px";
+
+body.append(grid);
+cont.append(grid);
+
+let k ;
+for (let i=0; i<n; i++) {
+    for (let j=0; j<m; j++) {
+      visited[i][j]=0;
+        k = document.createElement("cell");
+        k.id=`${i * m + j}`;
+        maze[i][j] = k;
+        maze[i][j].classList.add("grid");
+        body.append(k);
+        cont.append(k);
+        grid.append(k);
+        k.style.border = "1px solid #252423";
+        k.style.justifyContent = "center";
+        k.style.alignItems = "center";
+        k.style.display = "flex";
+        k.style.fontSize= "3rem";
+        k.style.fontWeight = "bold";
+        k.style.background = "#252423";
+        k.className="cell";
+    }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+let ord=[];
+
+function dfs (row, col, direction , parent) {
+  if (visited[row][col]==1){ 
+    return;
+  }
+  ord.push([row , col , 1 , direction, parent]);
+  visited[row][col]=1;
+    let arr=[1,2,3,4];
+    arr=shuffle(arr);
+    for (let i of arr) {
+      if (i == 1 && row > 0) dfs(row-1 , col , 1 , row * m + col);
+      if (i == 2 && row < n-1) dfs(row+1 , col , 2 , row * m + col);
+      if (i == 3 && col > 0) dfs(row , col-1 , 3 , row * m + col);
+      if (i == 4 && col < m-1) dfs(row , col+1 , 4 , row * m + col);
+    }
+    ord.push([row , col , -1]);
+  }
+
+dfs(0,0,2,0);
+let sz = ord.length;
+function animate(index , cid , type , direction , parent) {
+  if(index == sz) return;
+  if(type == -1)  
+  {
+    let borlef = document.getElementById(`${cid}`).style.borderLeftColor;
+    let borri = document.getElementById(`${cid}`).style.borderRightColor;
+    let borto = document.getElementById(`${cid}`).style.borderTopColor;
+    let borbo = document.getElementById(`${cid}`).style.borderBottomColor;
+    console.log(borlef , borri , borbo , borto);
+    if(borlef == "rgb(0, 214, 114)") 
+      document.getElementById(`${cid}`).style.borderLeft = "1px solid rgb(60,255,236)";
+    if(borri == "rgb(0, 214, 114)") 
+      document.getElementById(`${cid}`).style.borderRight = "1px solid rgb(60,255,236)";
+    if(borbo == "rgb(0, 214, 114)") 
+      document.getElementById(`${cid}`).style.borderBottom = "1px solid rgb(60,255,236)";
+    if(borto == "rgb(0, 214, 114)") 
+      document.getElementById(`${cid}`).style.borderTop = "1px solid rgb(60,255,236)";
+
+      console.log(borlef , borri , borbo , borto);
+
+    anime({
+    targets: document.getElementById(`${cid}`),
+    backgroundColor : `${type == 1 ? 'rgb(0 , 214 ,114)' : 'rgb(60,255,236)'}`,
+    scale: [
+      {value: .1, easing: 'easeOutSine', duration: 10},
+      {value: 1, easing: 'easeInOutQuad', duration: 10}
+    ],
+    duration : 10,
+    complete: ()=> {
+      if(index + 1 == sz) return;
+      animate(index + 1 , ord[index + 1][0] * m + ord[index + 1][1] , ord[index + 1][2] , ord[index + 1][3]
+        ,ord[index + 1][4]);
+    }});
+    return;
+  }
+
+  let borlef = document.getElementById(`${cid}`).style.borderLeft;
+  let borri = document.getElementById(`${cid}`).style.borderRight;
+  let borto = document.getElementById(`${cid}`).style.borderTop;
+  let borbo = document.getElementById(`${cid}`).style.borderBottom;
+
+  let Pborlef = document.getElementById(`${parent}`).style.borderLeft;
+  let Pborri = document.getElementById(`${parent}`).style.borderRight;
+  let Pborto = document.getElementById(`${parent}`).style.borderTop;
+  let Pborbo = document.getElementById(`${parent}`).style.borderBottom;
+
+  if(direction == 1) { borbo = "1px solid rgb(0 , 214 ,114)";} 
+  if(direction == 2) { borto = "1px solid rgb(0 , 214 ,114)" ;} 
+  if(direction == 3) { borri = "1px solid rgb(0 , 214 ,114)" ;} 
+  if(direction == 4) { borlef = "1px solid rgb(0 , 214 ,114)";} 
+  
+  if(direction == 1) { Pborto = "1px solid rgb(0 , 214 ,114)";} 
+  if(direction == 2) { Pborbo = "1px solid rgb(0 , 214 ,114)" ;} 
+  if(direction == 3) { Pborlef = "1px solid rgb(0 , 214 ,114)" ;} 
+  if(direction == 4) { Pborri = "1px solid rgb(0 , 214 ,114)";} 
+
+  // console.log(direction , cid , parent);
+  // console.log(borto , borbo , borlef , borri);
+  var also1 = anime({
+    targets : document.getElementById(`${parent}`),
+    borderBottom : Pborbo,
+    borderTop : Pborto,
+    borderLeft : Pborlef,
+    borderRight : Pborri,
+    duration : 2,
+  });
+
+  var myanimation = anime({
+    targets: document.getElementById(`${cid}`),
+    backgroundColor : `${type == 1 ? 'rgb(0 , 214 ,114)' : 'rgb(60,255,236)'}`,
+    scale: [
+      {value: .1, easing: 'easeOutSine', duration: 2},
+      {value: 1, easing: 'easeInOutQuad', duration: 2}
+    ],
+    borderBottom : borbo,
+    borderTop : borto,
+    borderLeft : borlef,
+    borderRight : borri,
+    duration : 2,
+    delay : 1,
+    complete: ()=> {
+      if(index + 1 == sz) return;
+      animate(index + 1 , ord[index + 1][0] * m + ord[index + 1][1] , ord[index + 1][2] , ord[index + 1][3]
+        ,ord[index + 1][4]);
+  }});
+
+  also1.play();
+  myanimation.play();
+}
+
+const staggersAnimation = anime.timeline({
+  targets: '.cell',
+  easing: 'easeInOutSine',
+  delay: anime.stagger(50),
+  loop: false,
+  autoplay: false
+})
+.add({
+  translateX: [
+    {value: anime.stagger('-.1rem', {grid : [n , m], from: 'center', axis: 'x'}) },
+    {value: anime.stagger('.1rem', {grid : [n , m], from: 'center', axis: 'x'}) }
+  ],
+  translateY: [
+    {value: anime.stagger('-.1rem', {grid : [n , m], from: 'center', axis: 'y'}) },
+    {value: anime.stagger('.1rem', {grid : [n , m], from: 'center', axis: 'y'}) }
+  ],
+  duration: 1000,
+  scale: .5,
+  delay: anime.stagger(100, {grid : [n , m], from: 'center'})
+})
+.add({
+  scaleY: 1,
+  scale: 1,
+  translateX : 0,
+  translateY : 0,
+  rotate : 0,
+  delay: anime.stagger(20, {grid : [n , m], from: 'center'}),
+  complete : () => {
+    animate(0 , ord[0][0] * m + ord[0][1] , 1 , ord[0][3] , ord[0][4]);
+  }
+})
+
+
+
+document.getElementById(`${n * m - 1}`).style.borderRight = "";
+document.getElementById(`${n * m - 1}`).style.borderBottom = "";
+
+const yo = document.querySelector('.left');
+yo.innerHTML += cont.innerHTML;
+cont.innerHTML = "";
+
+
+function clickHandler(event) {
+staggersAnimation.play();
+}
+
+const btn = document.querySelector('#go');
+btn.addEventListener('click', clickHandler);
